3f45687f68ef960740fc8dafc9cc1bd6e2837641
TheBestAstroNOT <139786546+TheBestAstroNOT@users.noreply.github.com>
2025-03-15 00:21:52 +0530

    Fixed crimanaplayer_setfile pattern for the msstore version of P3R

--- Ryo.Reloaded/CRI/Mana/CriManaPatterns.cs
+++ Ryo.Reloaded/CRI/Mana/CriManaPatterns.cs
@@ -6,7 +6,7 @@ internal static class CriManaGames
     {
         new("p3r")
         {
-            criManaPlayer_SetFile = "4C 89 44 24 ?? 48 89 54 24 ?? 48 89 4C 24 ?? 48 83 EC 38 48 83 7C 24 ?? 00 75 ?? 41 B8 FE FF FF FF 48 8D 15 ?? ?? ?? ?? 31 C9",
+            criManaPlayer_SetFile = "4C 89 44 24 ?? 48 89 54 24 ?? 48 89 4C 24 ?? 48 83 EC 38 48 83 7C 24 ?? 00 75 ?? 41 B8 B8 48 33 01",
         },
         new("metaphor")
         {
